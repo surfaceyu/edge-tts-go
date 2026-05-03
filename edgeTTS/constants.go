9f702eacbfaff8c3db3c4aafe056dd1f86b6c3b4
Maurits van der Schee <maurits@vdschee.nl>
2026-05-03 12:07:35 +0200

    fix bad handshake

--- edgeTTS/constants.go
+++ edgeTTS/constants.go
@@ -1,10 +1,16 @@
 package edgeTTS
 
+import "strings"
+
 const (
-	TRUSTED_CLIENT_TOKEN = "6A5AA1D4EAFF4E9FB37E23D68491D6F4"
-	WSS_URL              = "wss://speech.platform.bing.com/consumer/speech/synthesize/readaloud/edge/v1?TrustedClientToken=" + TRUSTED_CLIENT_TOKEN
-	VOICE_LIST           = "https://speech.platform.bing.com/consumer/speech/synthesize/readaloud/voices/list?trustedclienttoken=" + TRUSTED_CLIENT_TOKEN
+	TRUSTED_CLIENT_TOKEN  = "6A5AA1D4EAFF4E9FB37E23D68491D6F4"
+	WSS_URL               = "wss://speech.platform.bing.com/consumer/speech/synthesize/readaloud/edge/v1?TrustedClientToken=" + TRUSTED_CLIENT_TOKEN
+	VOICE_LIST            = "https://speech.platform.bing.com/consumer/speech/synthesize/readaloud/voices/list?trustedclienttoken=" + TRUSTED_CLIENT_TOKEN
+	CHROMIUM_FULL_VERSION = "143.0.3650.75"
+	SEC_MS_GEC_VERSION    = "1-" + CHROMIUM_FULL_VERSION
 )
+
+var CHROMIUM_MAJOR_VERSION = strings.SplitN(CHROMIUM_FULL_VERSION, ".", 2)[0]
 
 // Locale
 const (
